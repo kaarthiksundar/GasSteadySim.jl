84baf40896d8201adbd7da9f6fd4ad03464a67ae
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2021-09-02 14:11:36 -0600

    adds ig parsing

---
+++ src/core/bc.jl
@@ -0,0 +1,39 @@
+function build_bc(data::Dict{String,Any})::Dict{Symbol,Any}
+    bc = Dict{Symbol,Any}()
+
+    bc[:node] = Dict() 
+    bc[:compressor] = Dict()
+
+    if !haskey(data, "boundary_pslack") 
+        @error "Simulator requires at least one slack node pressure for uniqueness of solutions"
+    end 
+    
+    for (i, value) in get(data, "boundary_pslack", [])
+        id = parse(Int64, i)
+        bc[:node][id] = Dict( 
+            "val" => value, 
+            "control_type" => pressure_control
+        )
+    end 
+
+    for (i, value) in get(data, "boundary_nonslack_flow", [])
+        id = parse(Int64, i)
+        bc[:node][id] = Dict(
+            "val" => value,
+            "control_type" => flow_control
+        )
+    end 
+
+    for (i, value) in get(data, "boundary_compressor", [])
+        id = parse(Int64, i)
+        if value["control_type"] != 0 
+            @error "Simulator does not support compressor flow or discharge pressure specification for compressor boundary condition"
+        end 
+        bc[:compressor][id] = Dict(
+            "val" => value["value"],
+            "control_type" => value["control_type"]
+        )
+    end 
+
+    return bc
+end 
---
+++ src/core/ig.jl
@@ -0,0 +1,29 @@
+
+function build_ig(data::Dict{String,Any})::Dict{Symbol,Any}
+    ig = Dict{Symbol,Any}()
+
+    ig[:node] = Dict() 
+    ig[:pipe] = Dict()
+    ig[:compressor] = Dict()
+
+    for (i, value) in get(data, "nodal_pressure", [])
+        id = parse(Int64, i)
+        ig[:node][id] = value
+    end 
+
+    for (i, value) in get(data, "pipe_flow", [])
+        id = parse(Int64, i) 
+        ig[:pipe][id] = value 
+    end 
+
+    for (i, value) in get(data, "compressor_flow", [])
+        id = parse(Int64, i)
+        ig[:compressor][id] = value
+    end 
+
+    (isempty(ig[:node])) && (@info "initial guess for nodal pressure not provided") 
+    (isempty(ig[:pipe])) && (@info "initial guess for pipe flow not provided")
+    (isempty(ig[:compressor])) && (@info "initial guess for compressor flow not provided")
+
+    return ig
+end 
