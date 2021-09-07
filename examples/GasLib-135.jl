7a3419af571bf20c730a7a95759508dbcd9f35b5
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2021-09-07 16:01:49 -0600

    adds examples

---
+++ examples/GasLib-135.jl
@@ -0,0 +1,7 @@
+using GasSteadySim
+
+file = "./data/GasLib-135/"
+ss = initialize_simulator(file, eos=:full_cnga, initial_guess_filename="")
+solver_return = run_simulator!(ss)
+
+println(solver_return.status)
