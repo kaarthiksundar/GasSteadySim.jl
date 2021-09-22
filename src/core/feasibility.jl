da2b6cf5426286120c9decbe075b8dd85b29760f
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2021-09-22 13:34:27 -0600

    zero resistance check

--- src/core/feasibility.jl
+++ src/core/feasibility.jl
@@ -1,5 +1,5 @@
 function add_flow_bounds_to_ref!(ss::SteadySimulator)
-    for (_, pipe) in ref(ss, :pipe)
+    for (i, pipe) in ref(ss, :pipe)
         fr_node = pipe["fr_node"]
         to_node = pipe["to_node"]
         fr_node_p_min = ref(ss, :node, fr_node, "min_pressure")
@@ -11,6 +11,9 @@ function add_flow_bounds_to_ref!(ss::SteadySimulator)
         resistance = pipe["friction_factor"] * pipe["length"] * c / (2 * pipe["diameter"] * pipe["area"]^2)
     
         beta = 1/resistance
+        if isinf(beta) 
+            beta = 1e5
+        end
         p_sqr_max = fr_node_p_max^2 - to_node_p_min^2 
         p_cube_max = fr_node_p_max^3 - to_node_p_min^3 
 
