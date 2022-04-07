77e8423ec3539cababdc37811ed64c669b2bc789
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2022-04-06 20:25:46 -0600

    fix empty list issue

--- src/core/assemble.jl
+++ src/core/assemble.jl
@@ -34,8 +34,8 @@ function _eval_junction_equations!(ss::SteadySimulator, x_dof::AbstractArray, re
             r = (-val) # inflow is positive convention
             out_edge = ref(ss, :outgoing_dofs, node_id)
             in_edge = ref(ss, :incoming_dofs, node_id)
-            r -= sum(x_dof[e] for e in out_edge) 
-            r += sum(x_dof[e] for e in in_edge)
+            r -= sum(x_dof[e] for e in out_edge; init=0.0) 
+            r += sum(x_dof[e] for e in in_edge; init=0.0)
             residual_dof[eqn_no] = r
         end
     end
