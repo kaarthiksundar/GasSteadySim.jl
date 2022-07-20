a6d4b62ac3110f38e224c695110afae210c6a567
Shriram Srininavasan <shrirams@lanl.gov>
2022-07-20 09:14:36 -0600

    added commented lines to demonstrate how gmres solver can be passed

--- examples/8-node.jl
+++ examples/8-node.jl
@@ -1,8 +1,11 @@
 using GasSteadySim
-
 file = "./data/8-node/"
 ss = initialize_simulator(file, eos=:ideal, initial_guess_filename="")
+
+# using IterativeSolvers
+# solver_return = run_simulator!(ss, linsolve=(x, A, b) -> copyto!(x, gmres(A, b,  maxiter = 7000) ) )
 solver_return = run_simulator!(ss)
+
 
 println(solver_return.status)
 println(ref(ss, :node, 1, "withdrawal") * nominal_values(ss, :mass_flow))
