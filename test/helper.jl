67f2b461f4db60a83a32c5bbbfbe3764ba7f7de1
kaarthiksundar <kaarthik01sundar@gmail.com>
2022-11-03 14:54:19 -0600

    adds tol to tests

--- test/helper.jl
+++ test/helper.jl
@@ -2,7 +2,7 @@
 function _check_correctness(sol::Dict{String,Any}, exact_sol::Dict{String,Any}, tol = 1e-4)
     entries = ["compressor_flow", "pipe_flow", "nodal_pressure"]  
     for key in entries, i in sol[key] |> keys
-        @test get(sol[key], i, 0.0) ≈ get(exact_sol[key], string(i), 0.0) rtol = 1e-2
+        @test sol[key][i] ≈ exact_sol[key][string(i)] rtol = tol
     end
     return
 end
