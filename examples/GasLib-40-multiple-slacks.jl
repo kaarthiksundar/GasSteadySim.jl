e7b1cea63a4eaff798f1de039f0fecca578d2bf5
kaarthiksundar <kaarthik01sundar@gmail.com>
2023-01-26 12:30:26 -0700

    typo

--- examples/GasLib-40-multiple-slacks.jl
+++ examples/GasLib-40-multiple-slacks.jl
@@ -10,10 +10,8 @@ solver_return = run_simulator!(ss)
 println(solver_return.status)
 
 #============== Save solution data for use =============================#
-#=
-filename = "../test/data/GasLib-40-multiple-slacks/exact_sol_" * string(eos_var) * ".json"
-open(filename, "w") do f 
-        JSON.print(f, ss.sol, 2)
-end
-=#
-#=======================================================================#
+# filename = "../test/data/GasLib-40-multiple-slacks/exact_sol_" * string(eos_var) * ".json"
+# open(filename, "w") do f 
+#         JSON.print(f, ss.sol, 2)
+# end
+# #=======================================================================#
