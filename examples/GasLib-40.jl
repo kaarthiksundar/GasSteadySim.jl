559b9f2358a505cfbb50f23a90803b92b5404abd
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2025-05-30 10:10:13 -0600

    adds more options to GasLib40.jl

--- examples/GasLib-40.jl
+++ examples/GasLib-40.jl
@@ -3,9 +3,12 @@ using JSON
 
 file = "./data/GasLib-40/"
 eos_var = :ideal
-inertial_bool = false
-gravity_bool = false
-ss = initialize_simulator(file, eos=eos_var, initial_guess_filename="r.json")
+inertial_bool = true
+gravity_bool = true
+guess_file = "r1.json"
+write_bool = true
+write_file = "r4.json"
+ss = initialize_simulator(file, eos=eos_var, initial_guess_filename=guess_file)
 solver_return = run_simulator!(ss, gravity_bool= gravity_bool, inertial_bool=inertial_bool, iteration_limit=100, show_trace_flag=true)
 
 println(solver_return.status)
@@ -23,12 +26,14 @@ if solver_return.status != nl_solve_failure
                 r4 = pipe_equations_with_gravity_with_inertia(ss, solver_return.solution)
                 @show r4
         end
-
+        
+        if write_bool
+            filename = "./data/GasLib-40/" * write_file
+            open(filename, "w") do f 
+                JSON.print(f, ss.sol, 2)
+            end
+        end
         # filename = "./data/GasLib-40/exact_sol_ideal"  * ".json"
-        # open(filename, "w") do f 
-        #         JSON.print(f, ss.sol, 2)
-        # end
-        # filename = "./data/GasLib-40/r4"  * ".json"
         # open(filename, "w") do f 
         #         JSON.print(f, ss.sol, 2)
         # end
